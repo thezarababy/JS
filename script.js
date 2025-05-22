7dec47c648977cccfad304def0f29328a9522526
thezarababy <jyokoduwa@gmail.com>
2025-05-22 09:46:55 +0100

    js practice for group 2

---
+++ script.js
@@ -0,0 +1,48 @@
+function myData() {
+  //this is function declaration
+}
+myData();
+
+const myName = function () {
+  //this  is function expression
+};
+myName();
+
+const sayHello = () => {
+  //this is arrow function
+};
+sayHello();
+// calculate the area of a cone
+// 1/3 pie r square h
+// function areaOfACone(r, h) {
+//   answer = 1 / 3 * (Math.PI * r * h);
+//   console.log(answer);
+// }
+// areaOfACone(7,9);
+// quadratic equation
+// function quadraticEquation(a,b,c){
+//     let d = b**2 - 4*a*c;
+//     let rootOne=((-b+ Math.sqrt(d))/(2*a));
+//      let rootTwo=((-b-  Math.sqrt(d))/(2*a));
+//      console.log(`the first root is ${rootOne} the second root is ${rootTwo}`)
+
+// }
+// quadraticEquation(1,5,6);
+
+// function stringReverse() {
+//   string = " Good evening everyone";
+//   console.log(string.split(" ").reverse().join());
+// }
+// stringReverse();
+
+//write a function that returns the longest word in a sentence
+
+function longestWord(){
+    let whoAreYou ="we are begining javascript now";
+    console.log(whoAreYou.length)
+      console.log(whoAreYou.split(" "));
+      console.log(Math.max (whoAreYou.split(" ")[0].length, whoAreYou.split(" ")[1].length, whoAreYou.split(" ")[2].length, whoAreYou.split(" ")[3].length, whoAreYou.split(" ")[4].length));
+
+
+}
+longestWord();
